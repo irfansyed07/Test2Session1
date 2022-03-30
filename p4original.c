68a5b8455d4ab85597459685f1ef2f5839a37078
Irfan Syed <93091192+irfansyed07@users.noreply.github.com>
2022-03-30 08:21:39 +0000

    commit

---
+++ p4original.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+int input()
+{
+    int num;
+    printf("Enter n: ");
+    scanf("%d", &num);
+    return num;
+}
+int find_fibo(int n)
+{
+    int num1 = 0,num2 = 1,temp;
+    for (int i = 2; i <= n; i++)
+    {
+        temp = num1+num2;
+        num1 = num2;
+        num2 = temp;
+    }
+    return num2;
+}
+void output(int n, int fibo)
+{
+    printf("Fibo(%d) is %d\n", n, fibo);
+}
+int main()
+{
+    int num = input();
+    int res = find_fibo(num);
+    output(num, res);
+    return 0;
+}
